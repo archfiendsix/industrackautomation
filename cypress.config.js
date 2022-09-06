123c39b09b78147388b80d86f04df44a2ce98d8f
Ace Simon <ace.simon.g@gmail.com>
2022-09-06 18:20:42 +0800

    Added Report Generation through mochawesome and implemented combined report

--- cypress.config.js
+++ cypress.config.js
@@ -11,22 +11,22 @@ module.exports = defineConfig({
   integration: {
     baseUrl: "https://onetrack.industrack.com"
   },
-  "reporter": "mocha-allure-reporter",
-  // "reporterOptions": {
-  //   "reportDir": "cypress/report/mochawesome-report",
-  //   "reportFilename": "[status]_[datetime]-[name]-report",
-  //   "title": "[status]_[datetime]-[name]-report",
-  //   "timestamp": "longDate",
-  //   "charts": true,
-  //   "html": false,
-  //   "json": true,
-  //   "embeddedScreenshots":true,
-  //   "overwrite":true,
-  //   "inlineAssets": true
-  // },
-  // "compilerOptions": {
-  //   "types": ["jest", "node"]
-  // },
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/report/mochawesome-report",
+    "reportFilename": "[status]_[datetime]-[name]-report",
+    "title": "[status]_[datetime]-[name]-report",
+    "timestamp": "longDate",
+    "charts": true,
+    "html": false,
+    "json": true,
+    "embeddedScreenshots":true,
+    "overwrite":false,
+    "inlineAssets": true
+  },
+  "compilerOptions": {
+    "types": ["jest", "node"]
+  },
   defaultCommandTimeout: 8000,
   pageLoadTimeout: 30000,
   chromeWebSecurity: false,
